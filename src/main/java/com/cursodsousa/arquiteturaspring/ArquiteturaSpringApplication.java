741e16e86331dca4e83cdd4e86de274bb27d525c
Weslley Henrique Vieira Ferraz <weslleyhenrique800@gmail.com>
2025-06-03 17:25:58 -0400

    a

---
+++ src/main/java/com/cursodsousa/arquiteturaspring/ArquiteturaSpringApplication.java
@@ -0,0 +1,13 @@
+package com.cursodsousa.arquiteturaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArquiteturaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArquiteturaSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursodsousa/arquiteturaspring/ArquiteturaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursodsousa.arquiteturaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArquiteturaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
